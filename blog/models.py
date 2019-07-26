fad3bc7aad20545178dc9a9f3d179be6236e8fa8
Lakshmisaikumar <lakshmisaikumar.jampani2@mindtree.com>
2019-07-27 05:26:41 +0530

    Blog functionality

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,10 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:100]
 
-
-#migrate
-
-#add to the admin
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
